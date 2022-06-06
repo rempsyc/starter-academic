aa3fd1ef3fe596833d96f1c450d277df9b90ee73
RemPsyc <RemPsyc@users.noreply.github.com>
2022-06-06 10:12:42 -0400

    Create toc_template.R

---
+++ static/toc_template.R
@@ -0,0 +1,30 @@
+#' ---
+#' output:
+#'   html_document:
+#'     theme: cerulean
+#'     highlight: pygments
+#'     toc: yes
+#'     toc_depth: 3
+#'     toc_float: yes
+#'     number_sections: yes
+#'     df_print: kable
+#'     code_folding: show # or: hide
+#'     code_download: yes
+#' ---
+
+klippy::klippy(position = c('top', 'right'))
+
+#' # Introduction
+#' Here we will cover the head of mtcars.
+2+2
+#' # Nice Title
+#' ## Nice subtitle
+#' Just in a bit.
+print("hello world")
+#' # A third one
+#' ## Salutations
+#' ...matinales?
+#'
+#' ### Les plus cordiales
+#' There we go
+head(mtcars)
